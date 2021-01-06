1f4a1855a9364285eaeafaaeae13e2708c96329b
Ebraheem Ghantous <ave.brhom@gmail.com>
2021-01-06 16:26:49 +0200

    modify the timer

--- app.js
+++ app.js
@@ -1,158 +1,8 @@
-let Countries = [{
-        CountryName: "Israel",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_is-flag.gif",
-    },
-
-    {
-        CountryName: "Algeria",
-        FlagImg: " https://www.worldometers.info/img/flags/small/tn_ag-flag.gif ",
-    },
-    {
-        CountryName: "Bahrain",
-        FlagImg: " https://www.worldometers.info/img/flags/small/tn_ba-flag.gif",
-    },
-
-    {
-        CountryName: "Croatia",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_hr-flag.gif ",
-    },
-
-    {
-        CountryName: "Argentina",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ar-flag.gif  ",
-    },
-
-    {
-        CountryName: "Portugal",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_po-flag.gif ",
-    },
-
-    {
-        CountryName: "Australia",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_as-flag.gif  ",
-    },
-
-    {
-        CountryName: "Austria",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_au-flag.gif  ",
-    },
-
-    {
-        CountryName: "Canada",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ca-flag.gif",
-    },
-
-    {
-        CountryName: "Egypt",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_eg-flag.gif ",
-    },
-
-    {
-        CountryName: "Bahrain",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ba-flag.gif  ",
-    },
-
-    {
-        CountryName: "France",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_fr-flag.gif ",
-    },
-
-    {
-        CountryName: "Germany",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_gm-flag.gif  ",
-    },
-
-    {
-        CountryName: "Greece",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_gr-flag.gif ",
-    },
-
-    {
-        CountryName: "Belgium",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_be-flag.gif  ",
-    },
-
-    {
-        CountryName: "India",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_in-flag.gif ",
-    },
-
-    {
-        CountryName: "Italy",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_it-flag.gif ",
-    },
-
-    {
-        CountryName: "Japan",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ja-flag.gif ",
-    },
-
-    {
-        CountryName: "Jordan",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_jo-flag.gif ",
-    },
-
-    {
-        CountryName: "Brazil",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_br-flag.gif  ",
-    },
-
-    {
-        CountryName: "Kuwait",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ku-flag.gif  ",
-    },
-
-    {
-        CountryName: "Bulgaria",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_bu-flag.gif   ",
-    },
-
-    {
-        CountryName: "Lebanon",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_le-flag.gif  ",
-    },
-
-    {
-        CountryName: "Mexico",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_mx-flag.gif ",
-    },
-
-    {
-        CountryName: "Morocco",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_mo-flag.gif  ",
-    },
-
-    {
-        CountryName: "Canada",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ca-flag.gif",
-    },
-
-    {
-        CountryName: "China",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_ch-flag.gif   ",
-    },
-
-    {
-        CountryName: "Colombia",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_co-flag.gif",
-    },
-    {
-        CountryName: "Cuba",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_cu-flag.gif   ",
-    },
-
-    {
-        CountryName: "Denmark",
-        FlagImg: "https://www.worldometers.info/img/flags/small/tn_da-flag.gif   ",
-    },
-
-]; // array of objects that contain the flag picture url and the country name.
-
-
 let rand; // variable to store in it the random number.
 let flagName; //variable that store in it the flage name;
 let flagImg; // variable to store in it the url of the img;
 let score = 0; //deleare the score of the player;
+var timer;
 
 document.getElementById("playButton").addEventListener('click', function(event) {
     document.getElementById("home").style.display = "none";
@@ -172,8 +22,11 @@ document.getElementById("rulesButton").addEventListener("click", (event) => {
 })
 
 function getFlag() {
+    clearInterval(timer);
+    document.getElementById("timer").textContent = "" + 15;
     document.getElementById("TimeOut").style.display = "none";
     document.getElementById("theAnswer").disabled = false;
+    document.getElementById("submitB").disabled = false;
     rand = Math.floor(Math.random() * Countries.length);
     flagImg = document.getElementById("flagImg").src = Countries[rand].FlagImg;
     flagName = Countries[rand].CountryName;
@@ -190,16 +43,21 @@ document.getElementById("submitB").addEventListener('click', (event) => {
         document.getElementById("theAnswer").className = document.getElementById("theAnswer").className + " correct";
         score += 5;
         setTimeout(function() {
+            clearInterval(timer);
             getFlag();
+
         }, 1000)
 
     } else {
         document.getElementById("theAnswer").className = document.getElementById("theAnswer").className + " notCorrect";
+        document.getElementById("theAnswer").disabled = true;
+        document.getElementById("submitB").disabled = true;
         setTimeout(function() {
             document.getElementById("theGame").style.display = "none";
             document.getElementById("finalScore").style.display = "flex";
             document.getElementById("h1Score").textContent = "Your Score is : " + score;
-        }, 1000)
+
+        }, 1500)
     }
 });
 
@@ -220,25 +78,24 @@ document.getElementById("exitButton").addEventListener('click', (event) => {
 })
 
 function startCount() {
-    let f = 10;
-    let i;
-    for (i = 10; i >= 0; i--) {
+    let f = 14;
+    timer = setInterval(() => {
+        document.getElementById("timer").textContent = "" + f;
+        if (f == 0) {
+            document.getElementById("TimeOut").style.display = "inline";
 
-        setTimeout(() => {
-            document.getElementById("timer").textContent = "" + f;
-            if (f === 0) {
-                document.getElementById("TimeOut").style.display = "inline";
-                document.getElementById("theAnswer").disabled = true;
-                setTimeout(() => {
-                    document.getElementById("theGame").style.display = "none";
-                    document.getElementById("finalScore").style.display = "flex";
-                    document.getElementById("h1Score").textContent = "Your Score is : " + score;
-                }, 1500);
-            }
-            f--;
-        }, 1000 * i);
+            clearInterval(timer);
+            document.getElementById("theAnswer").disabled = true;
+            document.getElementById("submitB").disabled = true;
+            setTimeout(function() {
+                document.getElementById("theGame").style.display = "none";
+                document.getElementById("finalScore").style.display = "flex";
+                document.getElementById("h1Score").textContent = "Your Score is : " + score;
 
-    }
+            }, 1500)
+        }
+        f--;
+    }, 1000)
 
 
 }
